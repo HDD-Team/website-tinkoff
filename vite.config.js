ba3ae4c09567a27b4679308ea571781b4adc7ff2
HDD <gptsemail.com@gmail.com>
2024-06-02 04:00:59 +0300

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/website-tinkoff/', // Укажите ваш репозиторий
+  base: '/', // Укажите '/' если сайт будет развернут в корневом каталоге
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
